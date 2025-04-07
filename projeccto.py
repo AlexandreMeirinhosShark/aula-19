927ae08f0cad42e8bad6f82c8de460114b526f86
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2025-04-07 19:32:34 +0100

    Update projeccto.py

--- projeccto.py
+++ projeccto.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkintermapview as mapper
+import os
+from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("Portugal")
@@ -11,7 +13,10 @@ mapw = mapper.TkinterMapView(frame, width=800, height=600, corner_radius=0)
 
 mapw.set_position(39.81222, -7.99138)
 
-markr = mapw.set_marker(41.8060, -6.7567, text="Bragança")
+c_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+img_bragc = ImageTk.PhotoImage(Image.open(os.path.join(c_path, "images", "Posta-Mirandesa.webp")).resize((70, 70)))
+
+markr = mapw.set_marker(41.8060, -6.7567, text="Bragança", icon=img_bragc)
 
 mapw.set_zoom(7)
 mapw.pack()
